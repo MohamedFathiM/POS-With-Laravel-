98f43e22aab3c05495fb0a73ae7a9826f448814e
محمد فتحي <mohamedkdr66@yahoo.com>
2020-04-25 12:09:31 +0200

    add new migrations

---
+++ database/migrations/2020_04_24_031221_create_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->double('total_price', 8, 2)->nullable();
+            $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
